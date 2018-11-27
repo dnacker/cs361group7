358bc81e9c142c81fa214f5d7476e0617eb66848
David Ackerman <davidnackerman@gmail.com>
2018-11-26 23:06:29 -0800

    Basic phrase functionality operational.

--- PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/Phoneme.java
+++ PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/Phoneme.java
@@ -2,6 +2,7 @@ package com.pronunciation_match.pronunciationmatch;
 
 public class Phoneme {
     private String mText;
+    private String mDisplayText;
     private Tone mTone;
     private int mResourceId;
 
@@ -12,12 +13,17 @@ public class Phoneme {
     }
 
     private void setText(String text) {
+        mText = text;
         String[] prefixAndSuffix = StringUtils.parsePrefixAndSuffix(text);
-        mText = prefixAndSuffix[0] + StringUtils.getSuffixWithTone(prefixAndSuffix[1], mTone);
+        mDisplayText = prefixAndSuffix[0] + StringUtils.getSuffixWithTone(prefixAndSuffix[1], mTone);
     }
 
     public String getText() {
         return mText;
+    }
+
+    public String getDisplayText() {
+        return mDisplayText;
     }
 
     public Tone getTone() {
@@ -29,6 +35,6 @@ public class Phoneme {
     }
 
     public String toString() {
-        return getText();
+        return getDisplayText();
     }
 }
