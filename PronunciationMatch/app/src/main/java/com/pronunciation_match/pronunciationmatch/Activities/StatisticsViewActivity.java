c5aededa1c1d9ac5a7e23392aa56858f6984eddd
Keenan Taggart <keenanbtaggart@gmail.com>
2018-11-29 21:17:26 -0800

    Revert "Revert "Nav Drawer on all pages""
    This reverts commit 07d045373be175ed72ef6603c86c237a58724f35.

--- PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/Activities/StatisticsViewActivity.java
+++ PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/Activities/StatisticsViewActivity.java
@@ -1,9 +1,15 @@
 package com.pronunciation_match.pronunciationmatch.Activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -17,11 +23,53 @@ import com.pronunciation_match.pronunciationmatch.R;
 public class StatisticsViewActivity extends AppCompatActivity {
     private PhonemeAdapter phonemeAdapter;
     private PhonemeBank phonemeBank;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics_view);
+
+        mDrawerLayout = findViewById(R.id.stats_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.stats_nav_view);
+
+        // Reflect current page in nav drawer
+        Menu navDrawer = navigationView.getMenu();
+        MenuItem currentMenu = navDrawer.findItem(R.id.nav_stats);
+        currentMenu.setChecked(true);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        // item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        Intent i;
+                        switch(item.getItemId()) {
+                            case R.id.nav_stats:
+                                /*
+                                i = new Intent(StatisticsViewActivity.this, StatisticsViewActivity.class);
+                                startActivity(i);
+                                */
+                                break;
+                            case R.id.nav_phoneme_practice:
+                                i = new Intent(StatisticsViewActivity.this, PhonemeSelectionActivity.class);
+                                startActivity(i);
+                                break;
+                            case R.id.nav_phrase_practice:
+                                i = new Intent(StatisticsViewActivity.this, PhraseSelectionActivity.class);
+                                startActivity(i);
+                                break;
+                            case R.id.nav_recorder:
+                                i = new Intent(StatisticsViewActivity.this, RecordUser.class);
+                                startActivity(i);
+                                break;
+                        }
+                        return false;
+                    }
+                }
+
+        );
 
         ListView listView = findViewById(R.id.phonemeList);
         phonemeBank = PhonemeBank.getInstance();
