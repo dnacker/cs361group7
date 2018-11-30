c7a9fd1bc00ba71372d9b2b1e5d28ef52ae77d14
Keenan Taggart <keenanbtaggart@gmail.com>
2018-11-29 21:16:56 -0800

    Revert "Revert "Basic Nav Drawer""
    This reverts commit fc3401333a8c0ef1e3240204afe15c70cea1df95.

--- PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/Activities/PhonemeSelectionActivity.java
+++ PronunciationMatch/app/src/main/java/com/pronunciation_match/pronunciationmatch/Activities/PhonemeSelectionActivity.java
@@ -1,8 +1,13 @@
 package com.pronunciation_match.pronunciationmatch.Activities;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -20,11 +25,48 @@ public class PhonemeSelectionActivity extends AppCompatActivity {
     private PhonemeAdapter phonemeAdapter;
     private MediaPlayer mediaPlayer;
     private PhonemeBank phonemeBank;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phoneme_selection);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        Intent i;
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_stats:
+                                i = new Intent(PhonemeSelectionActivity.this, StatisticsViewActivity.class);
+                                startActivity(i);
+                                break;
+                            case R.id.nav_phoneme_practice:
+                                /*
+                                i = new Intent(PhonemeSelectionActivity.this, PhonemeSelectionActivity.class);
+                                startActivity(i);
+                                */
+                                break;
+                            case R.id.nav_phrase_practice:
+                                i = new Intent(PhonemeSelectionActivity.this, PhraseSelectionActivity.class);
+                                startActivity(i);
+                                break;
+                            case R.id.nav_recorder:
+                                i = new Intent(PhonemeSelectionActivity.this, RecordUser.class);
+                                startActivity(i);
+                                break;
+                        }
+                        return false;
+                    }
+                }
+        );
+
+
         ListView listView = findViewById(R.id.phoneme_list_view);
         phonemeBank = PhonemeBank.getInstance();
 
